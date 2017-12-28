a6bb524d8335376b01a735de2c561c9538945c2e
Angel Angeles <angelangelesiii@gmail.com>
2017-12-28 11:37:52 +0800

    Layout for main articles view page.

--- index.php
+++ index.php
@@ -17,40 +17,35 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+		<div class="wrapper-big no-pad pad-on-contact clearfix">
+			<div class="row collapse large-collapse medium-collapse small-collapse">
+				<div class="column large-8 small-12">
+					<?php
+					while ( have_posts() ) : the_post();
+			
+						get_template_part( 'template-parts/content', get_post_format() );
+			
+						// If comments are open or we have at least one comment, load up the comment template.
+						// if ( comments_open() || get_comments_number() ) :
+						// 	comments_template();
+						// endif;
+			
+					endwhile; // End of the loop.
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+					the_posts_navigation();
+					?>
+				</div>
+				<div class="column large-4 small-12">
+					<div class="sidebar-container article-sidebar">
+						<?php get_sidebar('1'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
