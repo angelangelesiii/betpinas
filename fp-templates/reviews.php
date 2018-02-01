3efc7f447795ff07743b1f3b21c4db2e11ac3170
Angel Angeles <angelangelesiii@gmail.com>
2018-02-01 10:56:34 +0800

    Added bonuses section in PBR

--- fp-templates/reviews.php
+++ fp-templates/reviews.php
@@ -46,7 +46,33 @@
             </section>
 
             <section class="tab-content" id="bonuses">
-
+            <!-- BEST BONUSES -->
+            <?php if(have_rows('pbr_bb','options')) :
+                while(have_rows('pbr_bb','options')) : the_row();
+            ?>
+                <article class="pbr-entry" style="background-color: <?php the_sub_field('panel_color') ?>; ">
+                    <header>
+                        <h3 class="entry-title"><?php the_sub_field('title'); ?></h3>
+                        <img class="entry-logo" src="<?php echo wp_get_attachment_image_url( get_sub_field('logo'), 'medium', false ) ?>" alt="">
+                    </header>
+                    <div class="entry-content clearfix">
+                        <div class="text-container">
+                            <?php the_sub_field('text'); ?>
+                            <div class="subtext"><?php the_sub_field('subtext'); ?></div>
+                        </div>
+                        <div class="btn-container">
+                            &nbsp;
+                            <a href="<?php the_sub_field('url') ?>" class="big-btn" target="_blank">
+                                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                                <span class="btn-text">GO!</span>
+                            </a>
+                        </div>
+                    </div>
+                </article>
+            
+            <?php endwhile;
+            endif; ?>
+            <!-- END BEST BONUSES -->
             </section>
         </div>
     
