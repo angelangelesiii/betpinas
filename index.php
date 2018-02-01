3383d07272febef761b36cbb4fdfa31b370ea6ff
Angel Angeles <angelangelesiii@gmail.com>
2018-02-01 20:00:20 +0800

    Fixed index articles pagination

--- index.php
+++ index.php
@@ -54,8 +54,7 @@ get_header(); ?>
 	
 						<?php
 						endwhile; // End of the loop.
-	
-						the_posts_navigation();
+						the_posts_pagination( array( 'mid_size' => 2 ) );
 						?>
 					</section>
 				<?php else: // If there are no posts ?>
