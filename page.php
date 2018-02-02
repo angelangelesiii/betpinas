332f13c25bc380148288f293496ae62f5a9cd196
Angel Angeles <angelangelesiii@gmail.com>
2018-02-02 20:15:58 +0800

    Page layout preliminary design

--- page.php
+++ page.php
@@ -17,22 +17,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
+		<div class="wrapper-big no-pad pad-on-contact clearfix">
+			<div class="row collapse large-collapse medium-collapse small-collapse">
+				<div class="column large-8 small-12">
+					<?php
+					while ( have_posts() ) : the_post();
+			
+						get_template_part( 'template-parts/content', get_post_type() );
+			
+						// the_post_navigation();
+			
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+			
+					endwhile; // End of the loop.
+					wpb_set_post_views(get_the_ID());
+					?>
+				</div>
+				<div class="column large-4 small-12">
+					<div class="sidebar-container article-sidebar">
+						<?php get_sidebar('2'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,42 +9,29 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+<article <?php post_class('article-container'); ?>>
+	<div class="banner-photo" style="background-image: url('<?php echo get_the_post_thumbnail_url( $post->ID, 'extra-large' ) ?>');">
+		<h1 class="title page-title"><?php the_title(); ?></h1>
+	</div>
+	<div class="article-proper">
+		<header class="article-header">
+			
+			<p class="meta">
+				<span class="author-category">
+					Posted by <?php if(get_the_author_firstname()) echo "<span class='a_name'>".get_the_author_firstname()."</span>"; if(get_the_author_lastname()) echo " <span class='a_name'>".get_the_author_lastname()."</span>"; ?><?php if(get_the_terms( $post->ID, 'sport') ): ?> in
+					<?php 
+					$sports = get_the_terms( $post->ID, 'sport');
+					echo "<span class='a_name'>".$sports[0]->name."</span>";
+					endif;?>
+					<br />
+				</span>
+				<span class="date"><?php the_date(); ?></span>
+			</p>
+			<?php edit_post_link( 'Edit this page' ); ?> 
+		</header>
+		<div class="content">
+			<?php the_content(); ?>
+		</div>
+	</div>
 
-	<div class="entry-content">
-		<?php
-			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'betpinas' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'betpinas' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
